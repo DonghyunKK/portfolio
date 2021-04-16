90a4ca20816b54b33967af303362100248e55296
donghyun kim <donghyunkim524@gmail.com>
2021-04-16 12:30:59 +0100

    add logo and style to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import { FaBars } from 'react-icons/fa'
+import logo from '../Logo.png'
 
 const NavBar = () => {
   return (
         <nav>
           <div className="nav-center">
             <div className="nav-header">
+              <img src={logo} alt="logo" className="logo" />
               <button className="nav-toggle">
                 <FaBars />
               </button>
             </div>  
-          </div>
-          <div className="links-container">
-            <NavLink to="/" exact className="links">
-              Home
-            </NavLink>
-            <NavLink to="/about" className="links">
-              About
-            </NavLink>
-            <NavLink to="/projects" className="links">
-              Projects
-            </NavLink>
+            <div className="links-container show-container">
+              <NavLink to="/" exact className="links">
+                Home
+              </NavLink>
+              <NavLink to="/about" className="links">
+                About
+              </NavLink>
+              <NavLink to="/projects" className="links">
+                Projects
+              </NavLink>
+            </div>
           </div>
         </nav>
   )
